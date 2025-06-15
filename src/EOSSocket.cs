d2f287a7cf3f6cf968fb395dc70c3be3ac172bdb
cryy22 <116713472+cryy22@users.noreply.github.com>
2025-06-15 15:05:18 -0700

    implement EOS socket

--- src/EOSSocket.cs
+++ src/EOSSocket.cs
@@ -1,10 +1,12 @@
 using System.Net;
 using System.Net.Sockets;
 using Backdash.Network.Client;
+using Epic.OnlineServices;
+using Epic.OnlineServices.P2P;
 
 namespace Backdash.EOS;
 
-public class EOSSocket : IPeerSocket
+public class EOSSocket(P2PInterface p2pInterface, SocketId socketId, EOSIdentity localIdentity) : IPeerSocket
 {
 	public AddressFamily AddressFamily => AddressFamily.Unspecified;
 	public int Port { get; }
@@ -15,21 +17,67 @@ public class EOSSocket : IPeerSocket
 		CancellationToken cancellationToken
 	)
 	{
-		return default;
+		var receivePacketOptions = new ReceivePacketOptions
+		{
+			LocalUserId = localIdentity.Id,
+			MaxDataSizeBytes = (uint) buffer.Length,
+		};
+
+		var result = p2pInterface.ReceivePacket(
+			ref receivePacketOptions,
+			out var senderId,
+			out var _,
+			out var _,
+			buffer.Span,
+			out var bytesWritten
+		);
+
+		if (result != Result.Success)
+		{
+			return ValueTask.FromException<int>(new Exception(result.ToString()));
+		}
+		ref var senderIdentity = ref EOSIdentityExtensions.FromSocketAddress(address);
+		senderIdentity.Id = senderId;
+
+		return ValueTask.FromResult((int) bytesWritten);
 	}
 
 	public ValueTask<SocketReceiveFromResult> ReceiveAsync(Memory<byte> buffer, CancellationToken cancellationToken)
 	{
-		return default;
+		throw new NotImplementedException();
 	}
 
-	public ValueTask<int> SendToAsync(
+	public unsafe ValueTask<int> SendToAsync(
 		ReadOnlyMemory<byte> buffer,
 		SocketAddress socketAddress,
 		CancellationToken cancellationToken
 	)
 	{
-		return default;
+		EOSIdentity recipientIdentity = EOSIdentityExtensions.FromSocketAddress(socketAddress);
+
+		fixed (byte* bytePtr = buffer.Span)
+		{
+			var sendPacketOptions = new SendPacketOptions
+			{
+				LocalUserId = localIdentity.Id,
+				RemoteUserId = recipientIdentity.Id,
+				SocketId = socketId,
+				Channel = 0,
+				AllowDelayedDelivery = false,
+				Reliability = PacketReliability.ReliableOrdered,
+				DisableAutoAcceptConnection = false,
+				Data = (nint) bytePtr,
+				DataLengthInBytes = (uint) buffer.Length,
+			};
+
+			var result = p2pInterface.SendPacket(ref sendPacketOptions);
+			if (result != Result.Success)
+			{
+				return ValueTask.FromException<int>(new Exception(result.ToString()));
+			}
+
+			return ValueTask.FromResult(buffer.Length);
+		}
 	}
 
 	public ValueTask<int> SendToAsync(
@@ -38,7 +86,7 @@ public class EOSSocket : IPeerSocket
 		CancellationToken cancellationToken
 	)
 	{
-		return default;
+		throw new NotImplementedException();
 	}
 
 	public void Close()
--- src/EOSSocketFactory.cs
+++ src/EOSSocketFactory.cs
@@ -1,12 +1,13 @@
 using Backdash.Network.Client;
 using Backdash.Options;
+using Epic.OnlineServices.P2P;
 
 namespace Backdash.EOS;
 
-public class EOSSocketFactory : IPeerSocketFactory
+public class EOSSocketFactory(P2PInterface p2pInterface, SocketId socketId, EOSIdentity localIdentity) : IPeerSocketFactory
 {
 	public IPeerSocket Create(int port, NetcodeOptions options)
 	{
-		return new EOSSocket();
+		return new EOSSocket(p2pInterface, socketId, localIdentity);
 	}
 }
