19f6962a98def9040635179a8cf4b782a71a3711
cryy22 <116713472+cryy22@users.noreply.github.com>
2025-07-15 01:59:16 -0700

    PURE UNBRIDLED HUBRIS

--- src/EOSSocket.cs
+++ src/EOSSocket.cs
@@ -11,26 +11,44 @@ public class EOSSocket(P2PInterface p2pInterface, SocketId socketId, ProductUser
 	public AddressFamily AddressFamily => AddressFamily.Unspecified;
 	public int Port { get; }
 
+	private byte[] _receiveAry = new byte[256];
+	private byte[] _sendAry = new byte[256];
+
 	public ValueTask<int> ReceiveFromAsync(
 		Memory<byte> buffer,
 		SocketAddress address,
 		CancellationToken cancellationToken
 	)
 	{
+		if (_receiveAry.Length < buffer.Length)
+		{
+			var targetLength = _receiveAry.Length;
+			while (targetLength > buffer.Length)
+			{
+				targetLength *= 2;
+			}
+
+			Array.Resize(ref _receiveAry, targetLength);
+		}
+		var receiveSeg = new ArraySegment<byte>(_receiveAry, 0, buffer.Length);
+
 		var receivePacketOptions = new ReceivePacketOptions
 		{
 			LocalUserId = productUserIds[localIdentity.IdIndex],
 			MaxDataSizeBytes = (uint) buffer.Length,
 		};
 
+		ProductUserId senderId = new ProductUserId();
+		SocketId socketId = new SocketId();
 		var result = p2pInterface.ReceivePacket(
 			ref receivePacketOptions,
-			out ProductUserId senderId,
-			out SocketId _,
+			ref senderId,
+			ref socketId,
 			out byte _,
-			buffer.Span,
+			receiveSeg,
 			out uint bytesWritten
 		);
+		receiveSeg.AsSpan().CopyTo(buffer.Span);
 
 		if (result == Result.NotFound)
 		{
@@ -67,32 +85,41 @@ public class EOSSocket(P2PInterface p2pInterface, SocketId socketId, ProductUser
 		CancellationToken cancellationToken
 	)
 	{
-		EOSIdentity recipientIdentity = EOSIdentityExtensions.FromSocketAddress(socketAddress);
-
-		fixed (byte* bytePtr = buffer.Span)
+		if (_sendAry.Length < buffer.Length)
 		{
-			var sendPacketOptions = new SendPacketOptions
+			var targetLength = _sendAry.Length;
+			while (targetLength > buffer.Length)
 			{
-				LocalUserId = productUserIds[localIdentity.IdIndex],
-				RemoteUserId = productUserIds[recipientIdentity.IdIndex],
-				SocketId = socketId,
-				Channel = 0,
-				AllowDelayedDelivery = false,
-				Reliability = PacketReliability.ReliableOrdered,
-				DisableAutoAcceptConnection = false,
-				Data = (nint) bytePtr,
-				DataLengthInBytes = (uint) buffer.Length,
-			};
-
-			var result = p2pInterface.SendPacket(ref sendPacketOptions);
-			if (result != Result.Success)
-			{
-				return ValueTask.FromException<int>(new Exception(result.ToString()));
+				targetLength *= 2;
 			}
 
-			Console.WriteLine("SEND successful");
-			return ValueTask.FromResult(buffer.Length);
+			Array.Resize(ref _sendAry, targetLength);
 		}
+		var sendSeg = new ArraySegment<byte>(_sendAry, 0, buffer.Length);
+		buffer.Span.CopyTo(sendSeg.AsSpan());
+
+		EOSIdentity recipientIdentity = EOSIdentityExtensions.FromSocketAddress(socketAddress);
+
+		var sendPacketOptions = new SendPacketOptions
+		{
+			LocalUserId = productUserIds[localIdentity.IdIndex],
+			RemoteUserId = productUserIds[recipientIdentity.IdIndex],
+			SocketId = socketId,
+			Channel = 0,
+			AllowDelayedDelivery = false,
+			Reliability = PacketReliability.ReliableOrdered,
+			DisableAutoAcceptConnection = false,
+			Data = sendSeg,
+		};
+
+		var result = p2pInterface.SendPacket(ref sendPacketOptions);
+		if (result != Result.Success)
+		{
+			return ValueTask.FromException<int>(new Exception(result.ToString()));
+		}
+
+		Console.WriteLine("SEND successful");
+		return ValueTask.FromResult(buffer.Length);
 	}
 
 	public ValueTask<int> SendToAsync(
